dc0011ce2fba7efe142490d633b39606bd83d69b
Srikanth Sombhatla <srikanthsombhatla@gmail.com>
2017-02-14 19:59:36 +1100

    morning: resetting before starting timer

--- commandcenter/skills/morning.js
+++ commandcenter/skills/morning.js
@@ -2,15 +2,15 @@ var Infra = require('../infra');
 var events = Infra.events;
 var speaker = Infra.speaker;
 var MINUTE = 1000 * 60;
+var REPEAT = 6;
 var delayInMs = 5 * MINUTE;
-var T = 6 * delayInMs;
+var T;
 var timeoutObj;
 
 var onTimeout =  function(context) {
   var inMins = T/MINUTE;
   var msg = "Master, you have " + inMins + " minutes to catch your train.";
   speaker.speak(msg);
-
   T = T - delayInMs;
   if (T <= 0) {
     console.log('clearing timer');
@@ -22,6 +22,7 @@ var onTimeout =  function(context) {
 var register = function() {
   events.on("schedule.workcountdown", function () {
       console.log("event has occured schedule.workcountdown");
+      T = (delayInMs * REPEAT);
       onTimeout('first shot');
       timeoutObj = setInterval(onTimeout, delayInMs, 'timer triggered');
   });
