8ffc4f29c0aa20d41a769f79a5d6284e10ba90de
Srikanth Sombhatla <totaramudu@Srikanths-MBP.gateway>
2017-07-30 21:00:59 +1000

    streamlined speak task callback

--- commandcenter/infra/speak.js
+++ commandcenter/infra/speak.js
@@ -5,9 +5,16 @@ var async = require('async');
 var _ = require('underscore');
 var polly = require('./aws-polly.js');
 
-var _speakTask = function(task, callback) {
+var _speakTask = function(task) {
     console.log('speaking:'+task.msg);
-    polly.speak(task.msg, callback);
+    var cb = task.cb;
+    polly.speak(task.msg, function(err) {
+      if (!err) {
+        cb(null, {'reply': 'spoken: '+task.msg});
+      } else {
+        cb(null, {'reply': 'unable to speak'});
+      }
+    });
     // var speed = _.has(task.options, 'speed') ? task.options.speed : 1.0;
     // var modulation = _.has(task.options, 'modulation') ? task.options.modulation : undefined;
     // say.speak(task.msg, modulation, speed, function(err) {
@@ -18,15 +25,13 @@ var _speakTask = function(task, callback) {
 }
 
 var speak = function(msg, options, cb) {
-    var task = {'msg': msg, 'options': options, 'cb': cb};    
+    var task = {'msg': msg, 'options': options, 'cb': cb};
     if (arguments.callee.speakingQueue === undefined) {
-        arguments.callee.speakingQueue = async.queue(function (task, callback) {
-            _speakTask(task, callback);
-            if (cb)
-                cb(null, {'reply': 'spoken: '+task.msg});
+        arguments.callee.speakingQueue = async.queue(function (task) {
+            _speakTask(task);
         }, 1);
         arguments.callee.speakingQueue.drain = function() {}
-    } 
+    }
     arguments.callee.speakingQueue.push(task);
 }
 
